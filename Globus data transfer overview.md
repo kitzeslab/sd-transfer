f55a6f1bed81279cf701578fb045890c84bca6cc
trieste22 <32990295+trieste22@users.noreply.github.com>
2019-03-22 11:39:32 -0400

    I/O, local vs Globus
    And improved documentation

--- Globus data transfer overview.md
+++ Globus data transfer overview.md
@@ -1,28 +1,72 @@
+README for use of sd-transfer.py
+Trieste Devlin, the Kitzes Lab, University of Pittsburgh, 03-2019
 
-Notes by Trieste, 02-2019, to document how we’ll be transferring data, using the script globus_sd_transfer.py
+This script can finds all SD cards named with a specified list of 1+ prefixes (ours are all called 'MSD-001', 'MSD-002, etc), and copy all of the files contained to the local or Globus cloud destination of your choice. The contents of each file is placed in its own folder with file structure and original write times maintained in the copies. A checksum is performed on each file to confirm successful transfer. There is the option to delete files from the SDs after successful transfer.
 
-SETUP:
-* Physical setup: any number of SD cards mounted via USB to a laptop on Ethernet for a fast internet connection. I have been using 1 or 2 Lexar 12-MSD hubs, connected via USB to my MacBook Pro
+Local usage is as simple as running as specified below with the -l flag. See below for Globus setup details, as there are a few changes you need to make in the python code to get synced up with your account.
+
+Note that this code is designed for use on Mac - some few changes to path structures are probably necessary to run on another OS. Hope this is useful!
+
+
+
+USAGE: copyfiles.py [-h] -p PREFIX -m MOUNTPATH -l LOCAL [-g GLOBUS] [-d]
+
+ARGUMENTS:
+
+  -h, --help            show this help message and exit
+
+  -p PREFIX, --prefix PREFIX
+                        Prefix(es) of all your SD cards' names. Enter multiple
+                        prefixes separated by spaces to indicate a range of
+                        prefixed names. [Required]
+
+  -m MOUNTPATH, --mountPath MOUNTPATH
+                        The path to where SD cards mount on this computer
+                        (defaults to Mac's mountpoint: /Volumes). [Optional]
+
+  -l LOCAL, --local LOCAL
+                        New local directory (with path) to save data to. [Required for local transfer]
+
+  -g GLOBUS, --globus GLOBUS
+                        New directory name (with absolute path) to upload data to in your
+                        Globus personal endpoint filesystem. [Required for Globus transfer]
+
+  -d, --delete          Delete files from SD cards after transfer and
+                        confirmation are complete. Files are only deleted if
+                        this flag is included. [Optional]
+
+EXAMPLE USAGE:
+
+   copyfiles.py -p MSD -l ~/Desktop/SD_folder -d 
+     # copy the contents of SD cards with names prefixed by "MSD" to "SD_folder" on your Desktop, then delete files from SDs
+   
+   copyfiles.py -p SD BobsData -g fieldData/sdTransfer
+     # copy the contents of SD cards with names prefixed by "SD" or "BobsData" to the folder "fieldData/sdTransfer" in your
+     # Globus Personal Endpoint filesystem, leaving the contents of the SD cards alone
+
+
+
+
+GLOBUS SETUP:
+
+* Physical setup: any number of SD cards mounted via USB to a laptop on Ethernet for a fast internet connection. Testing was done using variable size 2-12 MSD hubs connected via USB to a MacBook Pro.
 * Globus & script setup:
     * Follow [tutorial](https://docs.globus.org/how-to/globus-connect-personal-mac/ "Title") to download, install, & open Globus Connect Personal; get and enter key
-    * Update globus-sd-transfer.py with your Globus ID numbers (You shouldn’t have to change the pitt#dtn ID, but it’s in there too):
-        1. Client ID (your ‘app’) - Follow Steps 1 and 2 in this [tutorial](https://globus-sdk-python.readthedocs.io/en/stable/tutorial/ "blah") to make an ‘app’ (log in with your regular Pitt username and password, app name doesn’t matter) and copy the CLIENT_ID into _line 14_
-        2. Endpoint ID (your account) - log into Globus.org with your Pitt info, go to Endpoints/[you], and copy the UUID into _line 15_
-        3. pitt#dtn ID (where the data is going). Barry set up a symlink in the home directory of pitt#dtn (~/ibwo_data) for us to send this data to the Cluster.
-* Check that _line 10_ has the correct path to where the SDs mount (on Mac it’s /Volumes)
+    * Update globus-sd-transfer.py with your Globus ID numbers:
+        1. Client ID (your ‘app’) - Follow Steps 1 and 2 in this [tutorial](https://globus-sdk-python.readthedocs.io/en/stable/tutorial/ "blah") to make an ‘app’ (Pitt users: log in with your regular Pitt username and password, app name doesn’t matter) and copy the CLIENT_ID into _line 14_
+        2. Endpoint ID (your account) - log into Globus.org (Pitt users: with your Pitt login again), go to Endpoints/[you], and copy the UUID into _line 15_
+        3. dtn_ID (where the data is going). (Pitt users: talk to Trieste for deets or someone at CRC to get added)
 * Give Globus access to SD cards - Create a symlink to /Volumes (eg `ln -s /Volumes ~/Desktop/volsym`), then open Globus Connect Personal preferences (click the ‘g’ bubble in the top bar by wifi, for Mac), and make the symlink Sharable in the Access tab
 * Install Globus python SDK: 'pip install globus-sdk'
 
-USAGE:
-* Make sure you are logged into Globus and have access to pitt#dtn - go to the Endpoint in the online GUI and Activate Credentials with your Pitt user info if you’re not currently activated
-* Run script: python globus_sd_transfer.py
+GLOBUS USAGE:
+* Make sure you are logged into Globus and have access to the dtn_ID you'll be using (go to the Endpoint in the online GUI and Activate Credentials with your user info if it's not currently activated)
+* Run script: python sd-transfer.py with args as specified above (use absolute path for -g; \~ is interpreted as a local path)
 * Follow link to Consent Request using Globus page (cmd-click), enter any name in “Provide a label for future reference” box, click Allow
-* Copy Native App Authorization Code, paste into CL
-    * Note: script is set up to auto-reauth
+* Copy Native App Authorization Code, paste into commandline
 
-WEIRD CODE THINGS, for future work on this/other Globus scripts:
-* Instead of recursively copying from all of the MSD folders, I go into each MSD folder, and then add each file to the TransferData list. I did this so I could scan filenames and skip the invisible files (ie .Spotlight)
-* When specifying the path of local files/directories to be copied, you’ll get a permission error unless you have the FULL path included in the Globus API add_item() function. Ie Users/kitzeslab/Desktop, not \~ /Desktop. It doesn’t mind relative paths on the Globus side, though (\~/ibwo_data is fine)
-* “Recursively” copying directory /Foo/ will only recursively copy what’s inside /Foo/. I got around this by starting in /Volumes and recursively copying directories that begin with the prefix ‘MSD’. If SD files are ever named something else, changing the sd_prefix variable will handle the new names.
-* Globus will transfer a single symlinked file, but if you ask it to recursively transfer a symlink directory, it’ll just make an empty folder of the root name and not continue transferring further down the line.
 
+WEIRD CODE THINGS, for future reference on this/other Globus scripts:
+* When specifying the path of local files/directories to be copied from the Globus side, you’ll get a permission error unless you have the FULL path included in the Globus API add_item() function. Ie Users/me/Desktop, not \~ /Desktop. It doesn’t mind relative paths on the Globus side, though (\~/ibwo_data is fine). This is reversed for input on the local side - don't give it a relative Globus filepath.
+* “Recursively” copying directory /Foo/ will only recursively copy what’s inside /Foo/. I got around this by starting in /Volumes and recursively copying directories that begin with the one of the specified prefixes.
+* Globus will transfer a single symlinked file, but if you ask it to recursively transfer a symlink directory, it’ll just make an empty folder of the root name and not continue transferring further down the line. I thought this would be a brilliant way to recursively copy from every SD at once, but alas it had to be more complicated than that.
--- sd-transfer.py
+++ sd-transfer.py
@@ -1,58 +1,182 @@
-import globus_sdk
-import sys
 import os
-import re
 import datetime
+import time
+import re
+import shutil
+import sys
+import hashlib
+import argparse
 
-#usage: python globus_sd_transfer.py
+
+# calculate the md5 hash
+def getlocalfile_md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+# make a copy of files from sd to desired local destination, confirm data transfer with MD5 hash, then delete original from sd if desired
+def copyfile_local(fname, srcpath, dstpath, delete_choice):
+	if not fname.startswith('.'):
+
+		copied = dstpath + '/' + fname
+		command = 'cp -p ' + srcpath + '/' + fname + ' ' + dstpath
+		os.system(command)
+
+		md5local = getlocalfile_md5(srcpath+'/' + fname) 	 				# get hash for original file
+		md5sd = getlocalfile_md5(copied)									# get hash for new local copy
+
+		if not(md5local == md5sd):
+			print ('Oh no! Hash test failed for ' + dstpath +'/' + fname + '. Trying again.')
+			copyfile(fname, srcpath, dstpath)
+		else:
+			if delete_choice: # delete file from sd if user specified to
+				os.remove(srcpath + '/' + fname)
 
 
-sd_prefix = ['MSD', 'MDS', 'DMS', 'DSM', 'SDM', 'SMD'] # covering all our bases
-sd_mount = '/Volumes'
-now = datetime.datetime.now()
-current_date = '_' + str(now.month) + '-' + str(now.day) + '-' + str(now.year)
+def transfer_folder_contents(dst_path, sd_src_path, delete_choice): 
+	if not os.path.isdir(dst_path):
+		os.makedirs(dst_path, mode=0o777) # make directory with folders inside for each disk (or subdirectory)
 
-CLIENT_ID = 'bcb2d378-7ab1-4392-9f4e-7eee8b7a5e34' # app
-MYENDPOINT_ID = '7b24c79c-2fa0-11e9-9fa4-0a06afd4a22e' # UUID
-PITTDTN_ID = 'ee174383-87f2-11e5-995d-22000b96db58' # pitt#dtn
+		files = os.listdir(path=sd_src_path)
+		for file in files:
+			if not file.startswith('.'): #ignore hidden files
+				# print(sd_src_path + '/' + file)
+				if os.path.isdir(sd_src_path + '/' + file): #recursively copy nested folders
+					# print(str(file) + ' is a directory.')
+					local_subpath = dst_path + '/' + str(file)
+					sd_subpath = sd_src_path + '/' + str(file)
+					transfer_folder_contents(local_subpath, sd_subpath, delete_choice)#, local)
+				else: # bottom of the line. Copy file
+					copyfile_local(file, sd_src_path, dst_path, delete_choice)
 
-client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
-client.oauth2_start_flow(refresh_tokens=True)
 
-authorize_url = client.oauth2_get_authorize_url()
-print('Please go to this URL and login: {0}'.format(authorize_url))
+def local_transfer(sd_prefix, sd_mount, local_path, delete_choice):
+	sd_prefix # a list of 1+ is passed
+	if sd_prefix[0] == 'MSD':
+		sd_prefix.extend(['MDS', 'DMS', 'DSM', 'SDM', 'SMD'])
+	now = datetime.datetime.now()
+	current_date = '_' + str(now.month) + '-' + str(now.day) + '-' + str(now.year)
+	
+	disks = os.listdir(path=sd_mount) # SD cards mount to /Volumes on Mac
+	for disk in disks:
+		for i in range(len(sd_prefix)): # ignore hidden files, ie ".Spotlight-V100"
+			if bool(re.search(sd_prefix[i], disk)):			# iterate thru SD cards
+				folder_name = str(disk + current_date)
+				sd_fullpath = sd_mount + '/' + disk
+				local_fullpath = local_path + '/' + folder_name
 
-get_input = getattr(__builtins__, 'raw_input', input) # get correct input() fn to be compatible with Python2 or 3
-auth_code = get_input('Please enter the code you get after login here: ').strip()
-token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+				transfer_folder_contents(local_fullpath, sd_fullpath, delete_choice)#, 1)
 
-globus_auth_data = token_response.by_resource_server['auth.globus.org']
-globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+				print('\n     Files from ' + disk + ' copied to ' + local_fullpath + '.')
 
-# most specifically, you want these tokens as strings
-AUTH_TOKEN = globus_auth_data['access_token']
-TRANSFER_TOKEN = globus_transfer_data['access_token']
 
-# a GlobusAuthorizer is an auxiliary object we use to wrap the token. In more
-# advanced scenarios, other types of GlobusAuthorizers give us expressive power
-authorizer = globus_sdk.AccessTokenAuthorizer(TRANSFER_TOKEN)
-tc = globus_sdk.TransferClient(authorizer=authorizer)
-tdata = globus_sdk.TransferData(tc, MYENDPOINT_ID, PITTDTN_ID, label="", sync_level="checksum",preserve_timestamp=True,verify_checksum=True)
-#TODO: verify_checksum might create a bottleneck, but seems like a good idea
+def globus_upload(sd_p, sd_mount, upload_dir, delete_choice):
+	import globus_sdk
+	#only import if user needs - this will slow things down very slightly for Globus users, but save time for local users
 
-disks = os.listdir(path=sd_mount)					# SD cards mount to /Volumes on Mac
-for d in disks:
-	disk = d.upper() # guard against accidental bad naming
-	for i in range(len(sd_prefix)):
-		if bool(re.search(sd_prefix[i], disk)):			# iterate thru SD cards
-			new_folder = '/~/ibwo_data/'+str(disk + current_date)
-			sd_fullpath = sd_mount+'/' + disk
-			tc.operation_mkdir(PITTDTN_ID, path=new_folder) # new directory in ~/ibwo for each SD
-			files = os.listdir(path=sd_fullpath)
-			for file in files:
-				if not (bool(re.match("^\.",file))): # ignore hidden files
-					tdata.add_item(sd_fullpath+'/'+file, new_folder+'/'+file) # copy from SD to new Globus dir
+	sd_prefix = sd_p # a list of 1+ is passed
+	# sd_prefix.append(sd_p)
+	if sd_prefix[0] == 'MSD':
+		sd_prefix.extend(['MDS', 'DMS', 'DSM', 'SDM', 'SMD'])
+		# for kitzeslab naming conventions: covering all our bases in case a card name was mistyped
+	now = datetime.datetime.now()
+	current_date = '_' + str(now.month) + '-' + str(now.day) + '-' + str(now.year)
 
-transfer_result = tc.submit_transfer(tdata)
-print("task_id =", transfer_result["task_id"])
+	CLIENT_ID = '' # app
+	MYENDPOINT_ID = '' # UUID
+	DTN_ID = '' # dtn
+
+	client = globus_sdk.NativeAppAuthClient(CLIENT_ID)
+	client.oauth2_start_flow(refresh_tokens=True)
+
+	authorize_url = client.oauth2_get_authorize_url()
+	print('Please go to this URL and login: {0}'.format(authorize_url))
+
+	get_input = getattr(__builtins__, 'raw_input', input) # get correct input() fn to be compatible with Python2 or 3
+	auth_code = get_input('Please enter the code you get after login here: ').strip()
+	token_response = client.oauth2_exchange_code_for_tokens(auth_code)
+
+	globus_auth_data = token_response.by_resource_server['auth.globus.org']
+	globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+
+	# most specifically, you want these tokens as strings
+	AUTH_TOKEN = globus_auth_data['access_token']
+	TRANSFER_TOKEN = globus_transfer_data['access_token']
+
+	# a GlobusAuthorizer is an auxiliary object we use to wrap the token. In more
+	# advanced scenarios, other types of GlobusAuthorizers give us expressive power
+	authorizer = globus_sdk.AccessTokenAuthorizer(TRANSFER_TOKEN)
+	tc = globus_sdk.TransferClient(authorizer=authorizer)
+	tdata = globus_sdk.TransferData(tc, MYENDPOINT_ID, DTN_ID, label="", sync_level="checksum",preserve_timestamp=True,verify_checksum=True)
+
+	upload_dir = '~/' + upload_dir
+
+	tc.operation_mkdir(DTN_ID, path=upload_dir) # new directory in ~/ibwo for each SD
+	# you will error out if you specified a directory that already exists
+
+	disks = os.listdir(path=sd_mount)					# SD cards mount to /Volumes on Mac
+	for d in disks:
+		disk = d.upper() # guard against accidental bad naming
+		for i in range(len(sd_prefix)):
+			if bool(re.search(sd_prefix[i], disk)):			# iterate thru SD cards
+				new_folder = upload_dir + '/' + str(disk + current_date)
+				sd_fullpath = sd_mount+'/' + disk
+				tc.operation_mkdir(DTN_ID, path=new_folder) # new directory in indicated directory for each SD
+
+				files = os.listdir(path=sd_fullpath)
+				for file in files:
+					if not file.startswith("."): #ignore hidden files
+						if os.path.isdir(sd_fullpath + '/' + file): #recursively copy nested folders
+							tdata.add_item(sd_fullpath+ '/' +file, new_folder+'/'+file, recursive=True)
+						else:
+							tdata.add_item(sd_fullpath+'/'+file, new_folder+'/'+file) # copy from SD to new Globus dir
+
+	transfer_result = tc.submit_transfer(tdata)
+	print("task_id =", transfer_result["task_id"])
+
+
+###################################################################################### MAIN
+
+start = time.time()
+donemsg = 1
+local = 1
+
+parser = argparse.ArgumentParser(description='Copy files from SD cards to local storage or Globus cloud storage.')
+
+parser.add_argument("-p", "--prefix", nargs='+', required=True, help="Prefix(es) of all your SD cards' names. Enter multiple prefixes separated by spaces to indicate a range of prefixed names. [Required]")
+parser.add_argument("-m", "--mountPath", default='/Volumes', help ="The path to where SD cards mount on this computer (defaults to Mac's mountpoint: /Volumes). [Optional]")
+parser.add_argument("-l", "--local", default='0', help="New local directory name (with path) to save data to. [Required for local transfer]")
+parser.add_argument("-g", "--globus", help="New directory name (with absolute path) in your Globus filesystem to upload data to.[Required for local Globus transfer]")
+parser.add_argument("-d", "--delete", action='store_true', help="Delete files from SD cards after transfer and confirmation are complete. Files are only deleted if this flag is included. [Optional]")
+
+args = parser.parse_args()
+
+print("     SD prefix(es): ")
+for i in args.prefix:
+	print("        " + i)
+print("     SD mount path: " + args.mountPath)
+
+if(args.local != '0'):
+	print("     Saving to local directory: " + args.local)
+	local_transfer(args.prefix, args.mountPath, args.local, args.delete)
+if args.globus:
+	local = 0
+	print("     Uploading to directory " + args.globus + " on Globus.")
+	tmp = input('\n     Please confirm (Y/N) that you want to begin a Globus transfer, and have already updated the python script to include your Globus IDs (see README)\n     >>> ')
+	if tmp == 'Y':
+		globus_upload(args.prefix, args.mountPath, args.globus, args.delete)
+	else:
+		donemsg = 0
+		print('     Exiting.')
+if args.delete:
+	print("Files will be deleted off the SD card(s) after transfer and confirmation are finished.")
+
+if(donemsg):
+	if not local:
+		print('\nGlobus transfer initiated.\n')
+	else:
+		print ('\nDone with local transfer! Executed in ' + str(time.time()-start) + ' seconds\n')
+
 
